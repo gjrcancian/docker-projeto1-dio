70d6563f72b6e2fa761bc077ee606b14fa8feb9c
gjrcancian <gjrcancian@gmail.com>
2022-11-21 16:32:37 -0300

    pasta www

---
+++ www/index.php.php
@@ -0,0 +1,16 @@
+<!DOCTYPE HTML>
+	
+	<html lang=”pt-br”>
+		
+		<head>
+ 			<meta charset=”UTF-8”>
+  			<title>Docker Compose com Php</title>
+		</head>
+
+		<body>
+
+			<?php 
+				echo "Olá Mundo com Docker Compose + Php";
+			?>
+		</body>
+	</html>
